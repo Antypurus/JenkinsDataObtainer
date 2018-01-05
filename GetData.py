b9487508796a8f4572c8523f8c34dfd0c5fedb1f
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2018-01-05 03:00:04 +0000

    added class to hold user data for authentication and future reference

--- GetData.py
+++ GetData.py
@@ -1,9 +1,10 @@
 import urllib2
 import base64
+import sys
 
-def getJenkinsAPIData(url):
-    JENKINS_LOGIN = "admin"
-    JENKINS_PASSWD = "siroco3ds112"
+def getJenkinsAPIData(url,username,password):
+    JENKINS_LOGIN = username
+    JENKINS_PASSWD = password
 
     data = None
 
@@ -17,15 +18,49 @@ def getJenkinsAPIData(url):
     response = urllib2.urlopen(request)
     return response
 
+class connectionInfo:
+    
+    url = None
+    username = None
+    password = None
 
-url="http://localhost:8080/api/python?depth=0"
-response = getJenkinsAPIData(url)
+    def __init__(self):
+        self.queryURL()
+        self.queryUsename()
+        self.queryPassword()
+        return
+
+    def __init__(self,username,password,url):
+        self.url = url
+        self.password = password
+        self.username = username
+
+    def queryUsename(self):
+        name = None
+        sys.stdout.write("Username:")
+        name = sys.stdin.read()
+        if not name is None:
+            self.username = name
+
+    def queryPassword(self):
+        pasw = None
+        sys.stdout.write("Password:")
+        pasw = sys.stdin.read()
+        if not pasw is None:
+            self.password = pasw
+
+    def queryURL(self):
+        addr = None
+        sys.stdout.write("Server Address:")
+        addr = sys.stdin.read()
+        if not addr is None:
+            self.url = addr 
+
+
+
+response = getJenkinsAPIData("http://localhost:8080/api/python?depth=0")
 
 resp = eval(response.read())
 
-'''
-for key in resp:
-    print(key)
-'''
 data = resp["jobs"][0]
 print(resp["jobs"][0]["url"])
